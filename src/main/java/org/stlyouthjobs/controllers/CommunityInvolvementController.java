0080a15d32384cb25735a40dba57ca81d5844acd
Lillian <loveanime@hotmial.com>
2019-09-28 07:47:30 -0500

    3.5th commit: Skills and CommunityInvolvement controllers updated.

--- src/main/java/org/stlyouthjobs/controllers/CommunityInvolvementController.java
+++ src/main/java/org/stlyouthjobs/controllers/CommunityInvolvementController.java
@@ -2,9 +2,17 @@ package org.stlyouthjobs.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.stlyouthjobs.models.CommunityInvolvement;
 import org.stlyouthjobs.models.data.CommunityInvolvementDao;
+import org.stlyouthjobs.models.data.EducationDao;
+import org.stlyouthjobs.models.data.SkillsDao;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("communityInvolvement")
@@ -12,4 +20,30 @@ public class CommunityInvolvementController {
 
     @Autowired
     private CommunityInvolvementDao communityInvolvementDao;
+
+    @Autowired
+    private SkillsDao skillsDao;
+
+    @Autowired
+    private EducationDao educationDao;
+
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String addCommunityInvolvement(Model model){
+        model.addAttribute("title", "Add Community Involvement");
+        model.addAttribute(new CommunityInvolvement());
+
+        return "communityInvolvement/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processCommunityInvolvement(Model model, @ModelAttribute @Valid CommunityInvolvement communityInvolvement, Errors errors){
+        if (errors.hasErrors()){
+            model.addAttribute("title", "Add Community Involvement");
+            return "communityInvolvement/add";
+        }
+        communityInvolvementDao.save(communityInvolvement);
+
+        return "redirect:";
+    }
 }
--- src/main/java/org/stlyouthjobs/controllers/SkillsController.java
+++ src/main/java/org/stlyouthjobs/controllers/SkillsController.java
@@ -3,9 +3,15 @@ package org.stlyouthjobs.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.stlyouthjobs.models.Skills;
 import org.stlyouthjobs.models.data.SkillsDao;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("skills")
@@ -14,6 +20,20 @@ public class SkillsController {
     @Autowired
     private SkillsDao skillsDao;
 
-    @RequestMapping(value = "add", method= RequestMethod.GET)
-    public String skillsAdd(Model model)
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String addSkills(Model model){
+        model.addAttribute("title", "Add Skills");
+        model.addAttribute(new Skills());
+        return "skills/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processSkills(@ModelAttribute @Valid Skills newSkills, Errors errors, @RequestParam int skillsId, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Skills");
+            return "skills/add";
+        }
+        skillsDao.save(newSkills);
+        return "redirect:";
+    }
 }
