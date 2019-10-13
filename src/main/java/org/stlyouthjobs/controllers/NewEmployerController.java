10da13feed976075b995d15a60f2d10b29e18e37
Joseph Mirth <mirthlacrosse@gmail.com>
2019-10-13 17:10:13 -0500

    Deleted EmployerUser

--- src/main/java/org/stlyouthjobs/controllers/NewEmployerController.java
+++ src/main/java/org/stlyouthjobs/controllers/NewEmployerController.java
@@ -9,10 +9,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.stlyouthjobs.models.Address;
-import org.stlyouthjobs.models.EmployerUser;
 import org.stlyouthjobs.models.NewEmployer;
 import org.stlyouthjobs.models.data.EmployerAddressDao;
-import org.stlyouthjobs.models.data.EmployerUserDao;
 import org.stlyouthjobs.models.data.NewEmployerDao;
 
 import javax.validation.Valid;
@@ -25,9 +23,6 @@ public class NewEmployerController {
     private NewEmployerDao newEmployerDao;
 
     @Autowired
-    private EmployerUserDao employerUserDao;
-
-    @Autowired
     private EmployerAddressDao employerAddressDao;
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
@@ -38,11 +33,6 @@ public class NewEmployerController {
         model.addAttribute("ein", "EIN");
         model.addAttribute("phoneNumber", "Phone Number");
         model.addAttribute(new NewEmployer());
-        model.addAttribute("email", "E-mail");
-        model.addAttribute("confirmEmail", "Confirm E-mail");
-        model.addAttribute("password", "Password");
-        model.addAttribute("confirmPassword", "Confirm Password");
-        model.addAttribute(new EmployerUser());
         model.addAttribute("streetNumber", "Street Number");
         model.addAttribute("streetName", "Street Name");
         model.addAttribute("city", "City");
@@ -54,7 +44,7 @@ public class NewEmployerController {
     }
 
     @RequestMapping(value="add", method= RequestMethod.POST)
-    public String employerRegisterForm (Model model, @ModelAttribute("newemployer") @Valid NewEmployer newEmployer, @ModelAttribute Address address, @ModelAttribute EmployerUser employerUser,  Errors errors) {
+    public String employerRegisterForm (Model model, @ModelAttribute("newemployer") @Valid NewEmployer newEmployer, @ModelAttribute Address address, Errors errors) {
         if (errors.hasErrors()) {
             model.addAttribute("organizationName", "Organization Name");
             model.addAttribute("contactName", "Contact Name");
@@ -62,11 +52,6 @@ public class NewEmployerController {
             model.addAttribute("ein", "EIN");
             model.addAttribute("phoneNumber", "Phone Number");
             model.addAttribute(new NewEmployer());
-            model.addAttribute("email", "E-mail");
-            model.addAttribute("confirmEmail", "Confirm E-mail");
-            model.addAttribute("password", "Password");
-            model.addAttribute("confirmPassword", "Confirm Password");
-            model.addAttribute(new EmployerUser());
             model.addAttribute("streetNumber", "Street Number");
             model.addAttribute("streetName", "Street Name");
             model.addAttribute("city", "City");
@@ -77,7 +62,7 @@ public class NewEmployerController {
         }
         newEmployerDao.save(newEmployer);
         employerAddressDao.save(address);
-        newEmployerDao.save(newEmployer);
+
 
         return "redirect:/cheese";
     }
