79b5b0ca9ac63d3dd02d5f9f4875bddac4013166
Joseph Mirth <mirthlacrosse@gmail.com>
2019-10-24 19:58:05 -0500

    refactored New Employer

--- src/main/java/org/stlyouthjobs/models/NewEmployer.java
+++ src/main/java/org/stlyouthjobs/models/NewEmployer.java
@@ -42,36 +42,6 @@ public class NewEmployer {
     @Email
     private String confirmEmail;
 
-    @NotNull
-    @Size(max = 2500, message = "This field is required.")
-    private String password;
-
-    @NotNull
-    @Size(max = 2500, message = "This field is required.")
-    private String confirmPassword;
-
-    @NotNull
-    //@Size(min=1, max=10, message = "Street number must have at least one number.")
-    @Min(1)
-    private Integer streetNumber;
-
-    @NotNull
-    @Size(max = 2500, message = "This field is required.")
-    private String streetName;
-
-    @NotNull
-    @Size(max = 2500, message = "This field is required.")
-    private String city;
-
-    @NotNull
-    //@Size(min = 3, max = 9, message = "This field is required.")
-    //@Min(5)
-    //@Max(10)
-    private Integer zipCode;
-
-    @NotNull
-    @Size(max = 2500, message = "This field is required.")
-    private String neighborhood;
 
     @Id
     @GeneratedValue
@@ -145,62 +115,6 @@ public class NewEmployer {
 
     public void setConfirmEmail(String confirmEmail) {
         this.confirmEmail = confirmEmail;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
-    }
-
-    public Integer getStreetNumber() {
-        return streetNumber;
-    }
-
-    public void setStreetNumber(Integer streetNumber) {
-        this.streetNumber = streetNumber;
-    }
-
-    public String getStreetName() {
-        return streetName;
-    }
-
-    public void setStreetName(String streetName) {
-        this.streetName = streetName;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public Integer getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(Integer zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getNeighborhood() {
-        return neighborhood;
-    }
-
-    public void setNeighborhood(String neighborhood) {
-        this.neighborhood = neighborhood;
     }
 
 }
