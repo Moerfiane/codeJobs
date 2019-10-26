cc1f21c9bc01fb5c36ef5fda0cdd85e9c807d49d
Lillian <loveanime@hotmial.com>
2019-10-26 16:21:13 -0500

    updated ApplicationPortalController/app.java

--- src/main/java/org/stlyouthjobs/controllers/ApplicationPortalController.java
+++ src/main/java/org/stlyouthjobs/controllers/ApplicationPortalController.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.stlyouthjobs.models.App;
+import org.stlyouthjobs.models.Job;
 import org.stlyouthjobs.models.data.AppDao;
 
 import javax.validation.Valid;
@@ -34,6 +36,16 @@ public class ApplicationPortalController
 
         model.addAttribute("name", "Enter your name");
         model.addAttribute("jobTitle", "Enter name of job");
+        model.addAttribute("address", "Add Address");
+        model.addAttribute("jobCategory", "Select Job Category");
+        model.addAttribute("jobSummary", "Add Job Summary");
+        model.addAttribute("location", "Add Location");
+        model.addAttribute("schedule", "Add Schedule");
+        model.addAttribute("positionType", "Add Position Type");
+        model.addAttribute("numOfPositions", "Add Number of Positions");
+        model.addAttribute("dressCode", "Add Dress Code");
+        model.addAttribute("payRate", "Add Pay Rate");
+        model.addAttribute("closingDate", "Add Closing Date");
         model.addAttribute(new App());
 
         return "app/apply";
@@ -45,11 +57,58 @@ public class ApplicationPortalController
         if(errors.hasErrors()){
             model.addAttribute("name", "Enter your name");
             model.addAttribute("jobTitle", "Enter name of job");
+            model.addAttribute("address", "Add Address");
+            model.addAttribute("jobCategory", "Select Job Category");
+            model.addAttribute("jobSummary", "Add Job Summary");
+            model.addAttribute("location", "Add Location");
+            model.addAttribute("schedule", "Add Schedule");
+            model.addAttribute("positionType", "Add Position Type");
+            model.addAttribute("numOfPositions", "Add Number of Positions");
+            model.addAttribute("dressCode", "Add Dress Code");
+            model.addAttribute("payRate", "Add Pay Rate");
+            model.addAttribute("closingDate", "Add Closing Date");
             return "app/apply";
         }
         appDao.save(newApp);
 
         return "redirect:/app";
     }
+
+
+    @RequestMapping(value="apply/{jobId}", method=RequestMethod.GET)
+    public String displayEditJobForm(Model model, @PathVariable int appId) {
+
+        model.addAttribute("title", "Apply for Job");
+        model.addAttribute("app", appDao.findOne(appId));
+
+        return "app/apply";
+    }
+
+    @RequestMapping(value="apply/{jobId}", method = RequestMethod.POST)
+    public String processEditForm(Model model, @PathVariable int appId, @ModelAttribute  @Valid App newApp,
+                                  Errors errors) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Apply for Job");
+            return "app/apply";
+        }
+
+        App applyApp = appDao.findOne(appId);
+        applyApp.setJobTitle(newApp.getJobTitle());
+        applyApp.setAddress(newApp.getAddress());
+        applyApp.setJobCategory(newApp.getJobCategory());
+        applyApp.setLocation(newApp.getLocation());
+        applyApp.setSchedule(newApp.getSchedule());
+        applyApp.setJobSummary(newApp.getJobSummary());
+        applyApp.setPositionType(newApp.getPositionType());
+        applyApp.setNumOfPositions(newApp.getNumOfPositions());
+        applyApp.setDressCode(newApp.getDressCode());
+        applyApp.setPayRate(newApp.getPayRate());
+        applyApp.setClosingDate(newApp.getClosingDate());
+        appDao.save(applyApp);
+
+        return "redirect:/app";
+    }
+
 
 }
--- src/main/java/org/stlyouthjobs/models/App.java
+++ src/main/java/org/stlyouthjobs/models/App.java
@@ -1,8 +1,6 @@
 package org.stlyouthjobs.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -20,12 +18,54 @@ public class App {
     @NotNull
     private String jobTitle;
 
+    @NotNull
+    private String address;
+
+    @NotNull
+    private String jobCategory;
+
+    @NotNull
+    private String location;
+
+    @NotNull
+    private String schedule;
+
+    @NotNull
+    private String jobSummary;
+
+    @NotNull
+    private String positionType;
+
+    @NotNull
+    private String numOfPositions;
+
+    @NotNull
+    private String dressCode;
+
+    @NotNull
+    private Double payRate;
+
+    @NotNull
+    private String closingDate;
+
+
     //empty constructor
     public App(){}
 
-    public App(String name, String jobTitle) {
+    public App(String name, String jobTitle, String address, String jobCategory, String location, String schedule,
+               String jobSummary, String positionType, String numOfPositions, String dressCode, Double payRate, String closingDate) {
         this.name = name;
         this.jobTitle = jobTitle;
+        this.address = address;
+        this.jobCategory = jobCategory;
+        this.location = location;
+        this.schedule = schedule;
+        this.jobSummary = jobSummary;
+        this.positionType = positionType;
+        this.numOfPositions = numOfPositions;
+        this.dressCode = dressCode;
+        this.payRate = payRate;
+        this.closingDate = closingDate;
     }
 
     public int getId() {
@@ -46,5 +86,85 @@ public class App {
 
     public void setJobTitle(String jobTitle) {
         this.jobTitle = jobTitle;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getJobCategory() {
+        return jobCategory;
+    }
+
+    public void setJobCategory(String jobCategory) {
+        this.jobCategory = jobCategory;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(String schedule) {
+        this.schedule = schedule;
+    }
+
+    public String getJobSummary() {
+        return jobSummary;
+    }
+
+    public void setJobSummary(String jobSummary) {
+        this.jobSummary = jobSummary;
+    }
+
+    public String getPositionType() {
+        return positionType;
+    }
+
+    public void setPositionType(String positionType) {
+        this.positionType = positionType;
+    }
+
+    public String getNumOfPositions() {
+        return numOfPositions;
+    }
+
+    public void setNumOfPositions(String numOfPositions) {
+        this.numOfPositions = numOfPositions;
+    }
+
+    public String getDressCode() {
+        return dressCode;
+    }
+
+    public void setDressCode(String dressCode) {
+        this.dressCode = dressCode;
+    }
+
+    public Double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(Double payRate) {
+        this.payRate = payRate;
+    }
+
+    public String getClosingDate() {
+        return closingDate;
+    }
+
+    public void setClosingDate(String closingDate) {
+        this.closingDate = closingDate;
     }
 }
