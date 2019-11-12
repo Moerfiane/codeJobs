0d0de11394c8f9ec95d7559a954898b083500674
Monique <monique.jones712@gmail.com>
2019-11-11 20:27:44 -0600

    found some web config stuff. trying it out

--- src/main/java/org/CodeJobs/WebApplicationConfig.java
+++ src/main/java/org/CodeJobs/WebApplicationConfig.java
@@ -1,12 +1,17 @@
 package org.CodeJobs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @Configuration
+@EnableWebSecurity
 public class WebApplicationConfig extends WebMvcConfigurerAdapter {
 
     // Create managed bean to allow autowiring
@@ -18,6 +23,26 @@ public class WebApplicationConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor( authenticationInterceptor() );
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/index").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/register/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
     }
 
 }
