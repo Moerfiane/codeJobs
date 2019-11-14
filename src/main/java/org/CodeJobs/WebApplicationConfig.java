cf533b75550a7e51e7cd94e075094f0e7b585c98
Monique <monique.jones712@gmail.com>
2019-11-13 19:25:35 -0600

    more removals

--- src/main/java/org/CodeJobs/WebApplicationConfig.java
+++ src/main/java/org/CodeJobs/WebApplicationConfig.java
@@ -3,15 +3,15 @@ package org.CodeJobs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+/*import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;*/
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 public class WebApplicationConfig extends WebMvcConfigurerAdapter {
 
     // Create managed bean to allow autowiring
