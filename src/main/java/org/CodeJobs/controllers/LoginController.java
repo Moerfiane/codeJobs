de2e7db8c2b476956a526a54b27ad168550ec327
Monique <monique.jones712@gmail.com>
2020-01-12 22:27:56 -0600

    changed everything back to what it was before I shifted things for cloud deploy. I need to test it

--- src/main/java/org/CodeJobs/controllers/LoginController.java
+++ src/main/java/org/CodeJobs/controllers/LoginController.java
@@ -17,14 +17,14 @@ public class LoginController extends AbstractController{
     public String login(Model model) {
         model.addAttribute(new LoginForm());
         model.addAttribute("title", "Log In");
-        return "register/index";
+        return "register/login";
     }
 
     @RequestMapping(value = "login", method = RequestMethod.POST)
     public String login(@ModelAttribute @Valid LoginForm form, Errors errors, HttpServletRequest request) {
 
         if (errors.hasErrors()) {
-            return "register/index";
+            return "register/login";
         }
 
         User theUser = userDao.findByUsername(form.getUsername());
@@ -32,14 +32,14 @@ public class LoginController extends AbstractController{
 
         if (theUser == null) {
             errors.rejectValue("username", "user.invalid", "The given username does not exist");
-            return "register/index";
+            return "register/login";
         }
 
 
 
         if (!theUser.isMatchingPassword(password)) {
             errors.rejectValue("password", "password.invalid", "Invalid password");
-            return "register/index";
+            return "register/login";
         }
 
         setUserInSession(request.getSession(), theUser);
